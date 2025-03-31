1a99d8038c522b6d646151de829515fb67c69bb3
Hakimttout <82940676+Hakimttout@users.noreply.github.com>
2025-03-31 22:59:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ apt install -y python3-venv nginx certbot python3-certbot-nginx
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirement.txt
 
 echo "Redéploiement de Streamlit"
 nohup streamlit run app/frontend/streamlit_app.py --server.port 8888 --server.address localhost &
